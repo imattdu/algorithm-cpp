34430e50b653acdf613d4fdf8a99c6e64a5a44e9
matt <imattdu@gmail.com>
2022-03-03 09:09:31 +0800

    添加prim算法

---
+++ base/33/t1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int N = 510, INF = 0x3f3f3f3f;
+int g[N][N], dist[N];
+bool st[N];
+int n, m, a, b, w;
+
+
+int prim() {
+    memset(dist, 0x3f, sizeof dist);
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int t = -1;
+        for (int j = 1; j <= n; j++) {
+            if (!st[j] && (t == -1 || dist[j] < dist[t])) t = j;
+        }
+
+        if (i && dist[t] == INF) return INF;
+        if (i) res += dist[t];
+        st[t] = true;
+        for (int j = 1; j <= n; j++) dist[j] = min(dist[j], g[t][j]);
+    }
+
+    return res;
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i != j) g[i][j] = INF;
+        }
+    }
+
+    while (m--) {
+        cin >> a >> b >> w;
+        g[a][b] = g[b][a] = min(g[a][b], w);
+    }
+
+    int res = prim();
+    if (res == INF) puts("impossible");
+    else cout << res;
+    return 0;
+
+}
